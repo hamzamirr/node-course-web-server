1749b0d1eba28d81ba13e8b6198c8d296b06bbd9
hamza mir <hamxa46.mir@gmail.com>
2017-08-19 17:02:25 +0500

    project page added to the home page

--- app.js
+++ app.js
@@ -12,10 +12,6 @@ app.use(express.static(__dirname + '/public'));
 
 
 app.use((req,res,next)=>{
-    res.render('workinprogress.hbs');
-    });
-
-app.use((req,res,next)=>{
     var now=new Date().toDateString();
    var log= `${now}, ${req.method}, ${req.url}`;
     console.log(log);
@@ -40,12 +36,26 @@ app.get('/',(req,res)=>{
     
 });
 
+
+app.get('/projects',(req,res)=>{
+    res.render('projects.hbs',{
+        title:'projects'
+    });
+});
+
+
 app.get('/home1',(req,res)=>{
     res.render('home1.hbs',{
         name:'hamza',
         header:'HBS'
     }); 
 });
+
+
+app.use((req,res,next)=>{
+    res.render('workinprogress.hbs');
+    });
+
 
 app.listen(port,(err)=>{
     if(err){
